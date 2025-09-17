718094f65df5890d5ffc00d81096fea14f453368
Kolade Faith <faithkolade0507@gmail.com>
2025-09-17 13:07:02 +0100

    Changes made in PRoduct component

--- src/components/CartPersistence.jsx
+++ src/components/CartPersistence.jsx
@@ -17,7 +17,8 @@ const CartPersistence = () => {
     }
   }, [user, setCart]);
 Lorem ipsum dolor sit amet consectetur adipisicing elit.Praesentium, architecto fugit.Tempore, est vitae repellendus, delectus natus quibusdam unde, illum amet iste ea impedit consequatur atque voluptate ? Dolor, nostrum velit.
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore dolorum officia culpa, necessitatibus nemo aspernatur quod reprehenderit ipsum aliquam et velit deleniti, sint, corrupti ipsa magnam tenetur inventore eveniet.
+  Lorem ipsum dolor sit amet consectetur adipisicing elit.Optio tempore dolorum officia culpa, necessitatibus nemo aspernatur quod reprehenderit ipsum aliquam et velit deleniti, sint, corrupti ipsa magnam tenetur inventore eveniet.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur placeat, autem ipsum, sapiente consectetur nemo sint eius reprehenderit inventore deleniti animi voluptatum quas repellendus illum incidunt corporis iusto, officia dolor?
   // Save cart when it changes (only for logged-in users)
   useEffect(() => {
     if (user && cart.length >= 0) {
